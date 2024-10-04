80f9127babb7aa205e31cf1f6378b972f2a259b7
NuryevArtem <Tyuiu.NuryevAR@outlook.com>
2024-10-04 18:42:10 +0500

    Task0

---
+++ Tyuiu.NuryevAR.Sprint1.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.NuryevAR.Sprint1.Task0.V25.Lib;
+
+namespace Tyuiu.NuryevAR.Sprint1.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}
